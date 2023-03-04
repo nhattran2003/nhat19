66612ca42cb46e6e73a16b22899e097098d1b30d
nhattran2003 <126789226+nhattran2003@users.noreply.github.com>
2023-03-04 17:57:50 +0700

    Add files via upload

---
+++ 2.7.cpp
@@ -0,0 +1,14 @@
+struct node
+{
+    int info;
+    struct node *pNext;
+    struct node *pPrev;
+};
+typedef struct node NODE;
+
+struct list
+{
+    NODE *pHead;
+    NODE *pPrev;
+};
+typedef struct list LIST;
